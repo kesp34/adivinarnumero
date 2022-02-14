244489ff90cf833f90aa4b8fc10ff66285de4427
Jose Luis <jose245198@gmail.com>
2022-02-14 13:53:40 +0100

    Agregar archivos de proyecto.

---
+++ potencias/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace adivinarNumero
+{
+    public class Prueba
+    {
+        public static void Main()
+        {
+            int n1, n2, contador;
+
+            n2 = 7;
+            contador = 1;
+            Console.Write("Adivina un número del 1 al 10");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            while ((n1 != n2) && (contador < 3))
+            {
+                Console.WriteLine("Número incorrecto");
+
+                Console.Write("Intenta de nuevo, Adivina un número del 1 al 10:");
+                n1 = Convert.ToInt32(Console.ReadLine());
+
+                contador++;
+            }
+
+            if (n1 == n2)
+                Console.WriteLine("Has acertado");
+            else
+                Console.WriteLine("Has acabado tus intentos");
+        }
+    }
+}
